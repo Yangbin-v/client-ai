279c279858c94d31e582f660edd532e2b59f2d69
Yangbin-v <2517726788@qq.com>
2025-01-07 22:52:02 +0800

    还差构建模型运行参数

--- src/ai/index.ts
+++ src/ai/index.ts
@@ -1,19 +1,23 @@
 import * as ort from 'onnxruntime-web';
 import {getModelInput} from './util';
 
+ort.env.wasm.wasmPaths = '/';  // WASM 文件在 public 目录下
+
 /** 模型 */
 class Model {
     // @ts-expect-error TextGenerationPipeline 允许缺省
     private session: ort.InferenceSession;
 
     async init() {
-        this.session = await ort.InferenceSession.create('./model/model.onnx');
+        this.session = await ort.InferenceSession.create('/model/model.onnx');
     }
 
     async chat(prompt: string) {
         if (!this.session) {
             await this.init();
         }
+
+        console.log('必要输入', this.session.inputNames);
 
         const inputData = await getModelInput(prompt);
 
